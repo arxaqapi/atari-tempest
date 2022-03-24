66950b43979928320d2ea26596d92180360c3cc9
Massimo VENUTI <massimo.venuti@etu.unistra.fr>
2022-03-24 09:41:47 +0100

    using active field to render or not game objects

--- src/core/Blaster.cpp
+++ src/core/Blaster.cpp
@@ -7,6 +7,8 @@
 void
 Blaster::render(SDL_Renderer* renderer, const Map& map) const
 {
+  if (!active_)
+    return;
   Vector2D p = map.calcPosition(band_num_, progress_);
   // draw a rectangle for now...
   SDL_Rect rect;
--- src/core/Flipper.cpp
+++ src/core/Flipper.cpp
@@ -10,6 +10,8 @@ Flipper::Flipper() : GameObject(1)
 void
 Flipper::render(SDL_Renderer* renderer, const Map& map) const
 {
+  if (!active_)
+    return;
   Vector2D p = map.calcPosition(band_num_, progress_);
   // draw a rectangle for now...
   SDL_Rect rect;
