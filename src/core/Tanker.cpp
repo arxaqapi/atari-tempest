9890298a03493d5f8c2a1a3e42a5b51afbd080c5
Massimo VENUTI <massimo.venuti@etu.unistra.fr>
2022-04-23 22:27:35 +0200

    improving tanker rendering

--- src/core/Tanker.cpp
+++ src/core/Tanker.cpp
@@ -18,23 +18,87 @@ Tanker::render(SDL_Renderer* renderer, const Map& map) const
 
   SDL_SetRenderDrawColor(renderer, 230, 0, 0, 255);
 
-  // Draw rect for now...
+  const Band& band = map.getBand(band_num_);
+
   f32 fraction =
     utils::easeOutQuad(front_progression_, 1 - map.getFocal()) - map.getFocal();
 
-  const Band& band = map.getBand(band_num_);
+  f32 inner_size = (30 * (1 - fraction)) / 2;
+  f32 outer_size = (60 * (1 - fraction)) / 2;
 
-  Vector2D position = band.getExterCenter().vec_to(map.getOrigin()) * fraction +
-                      band.getExterCenter();
+  Vector2D u = {1,0};
+  Vector2D v = {0,1};
+  Vector2D position = band.getExterCenter().vec_to(map.getOrigin()) * fraction;
 
-  //    Vector2D position =
-  //      band.getExterCenter().weightedMidPointTo(map.getOrigin(), fraction);
+  Vector2D inner_left = band.getExterCenter() + (u * .7f * inner_size ) + position;
+  Vector2D inner_right = band.getExterCenter() + (u * (-.7f*inner_size))+ position;
+  Vector2D inner_top = band.getExterCenter() + (v * inner_size) + position;
+  Vector2D inner_bottom = band.getExterCenter() + (v * -inner_size) + position;
+  Vector2D outer_left = band.getExterCenter() + (u * (.7f*outer_size)) + position;
+  Vector2D outer_right = band.getExterCenter() + (u * (-.7f*outer_size))+ position;
+  Vector2D outer_top = band.getExterCenter() + (v * outer_size) + position;
+  Vector2D outer_bottom = band.getExterCenter() + (v * -outer_size) + position;
 
-  f32 size = 20 * (1 - fraction);
-
-  SDL_FRect rect{ position.getX(), position.getY(), size, size };
-
-  SDL_RenderFillRectF(renderer, &rect);
+  SDL_RenderDrawLineF(renderer,
+                      inner_left.getX(),
+                      inner_left.getY(),
+                      inner_top.getX(),
+                      inner_top.getY());
+  SDL_RenderDrawLineF(renderer,
+                      inner_top.getX(),
+                      inner_top.getY(),
+                      inner_right.getX(),
+                      inner_right.getY());
+  SDL_RenderDrawLineF(renderer,
+                      inner_right.getX(),
+                      inner_right.getY(),
+                      inner_bottom.getX(),
+                      inner_bottom.getY());
+  SDL_RenderDrawLineF(renderer,
+                      inner_bottom.getX(),
+                      inner_bottom.getY(),
+                      inner_left.getX(),
+                      inner_left.getY());
+  SDL_RenderDrawLineF(renderer,
+                      outer_left.getX(),
+                      outer_left.getY(),
+                      outer_top.getX(),
+                      outer_top.getY());
+  SDL_RenderDrawLineF(renderer,
+                      outer_top.getX(),
+                      outer_top.getY(),
+                      outer_right.getX(),
+                      outer_right.getY());
+  SDL_RenderDrawLineF(renderer,
+                      outer_right.getX(),
+                      outer_right.getY(),
+                      outer_bottom.getX(),
+                      outer_bottom.getY());
+  SDL_RenderDrawLineF(renderer,
+                      outer_bottom.getX(),
+                      outer_bottom.getY(),
+                      outer_left.getX(),
+                      outer_left.getY());
+  SDL_RenderDrawLineF(renderer,
+                      outer_top.getX(),
+                      outer_top.getY(),
+                      inner_left.getX(),
+                      inner_left.getY());
+  SDL_RenderDrawLineF(renderer,
+                      outer_right.getX(),
+                      outer_right.getY(),
+                      inner_top.getX(),
+                      inner_top.getY());
+  SDL_RenderDrawLineF(renderer,
+                      outer_bottom.getX(),
+                      outer_bottom.getY(),
+                      inner_right.getX(),
+                      inner_right.getY());
+  SDL_RenderDrawLineF(renderer,
+                      outer_left.getX(),
+                      outer_left.getY(),
+                      inner_bottom.getX(),
+                      inner_bottom.getY());
 }
 
 void
