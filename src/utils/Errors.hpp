93c343f8704ebfb3d37390753a89642039a9f10f
arxaqapi <arxaqapi@protonmail.com>
2022-04-26 17:42:48 +0200

    feat: doc des erreurs

--- src/utils/Errors.hpp
+++ src/utils/Errors.hpp
@@ -15,31 +15,64 @@
 #include "types.hpp"
 #include <stdexcept>
 
+/**
+ * @brief Erreurs personnalisées afin de faciliter le développement
+ *
+ */
 namespace errors {
+/**
+ * @brief A utiliser dans le cas ou la méthode n'a pas encore été implémentée
+ *
+ */
 struct not_implemented : public std::exception
 {
   const char* what() const throw();
 };
+
+/**
+ * @brief A utiliser dans le cas ou la méthode ou un certain comportement
+ * n'a pas encore été testée
+ *
+ */
 struct not_tested : public std::exception
 {
   const char* what() const throw();
 };
 
+/**
+ * @brief A utiliser dans le cas ou le changement d'état demandé
+ * n'est pas autorisée par la machine à état
+ *
+ */
 struct non_valid_state_switch : public std::exception
 {
   const char* what() const throw();
 };
 
+/**
+ * @brief A utiliser dans le cas ou le fichier n'a pas pu ou n'a pas été ouvert
+ *
+ */
 struct file_not_opened : public std::exception
 {
   const char* what() const throw();
 };
 
+/**
+ * @brief A utiliser dans le cas ou le caractère demandé
+ * n'est pas supporté par la police d'écriture actuelle
+ *
+ */
 struct non_valid_character_requested : public std::exception
 {
   const char* what() const throw();
 };
 
+/**
+ * @brief A utiliser afin de wrapper les messages d'érreurs de la SDL
+ * ou pour exprimer une erreur propre à la SDL
+ *
+ */
 struct sdl_error : public std::exception
 {
   std::string msg_;
