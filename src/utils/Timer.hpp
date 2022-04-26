5c8465eed1f0d77fed95c8019d61c10eab6d542e
arxaqapi <arxaqapi@protonmail.com>
2022-04-26 18:01:29 +0200

    feat: doc de la classe Timer

--- src/utils/Timer.hpp
+++ src/utils/Timer.hpp
@@ -17,12 +17,22 @@
 #include "types.hpp"
 #include <random>
 
+/**
+ * @brief Type du timer utilisé, le timer PERFORMANCE utilise
+ * une horloge de plus grande précision
+ *
+ */
 enum class timer_type
 {
   NORMAL,
   PERFORMANCE
 };
 
+/**
+ * @brief Classe permettant de gérer la gestion du temps
+ * et donc du nombre d'images par secondes de notre jeux
+ *
+ */
 class Timer
 {
 private:
@@ -37,35 +47,71 @@ private:
 
 public:
   /**
-   * @brief Construct a new Timer object that will
-   * count the number of milliseconds passed
+   * @brief Construction d'un nouvel objet Timer normal permettant de compter
+   * le nombre de millisecondes qui sont passées
+   * tout en limitant le temps maximum utilisé par images
    *
+   * @param max_ms_per_frame nombre maximum de millisecondes autorisées par
+   * image
    */
   Timer(u32 max_ms_per_frame);
+  /**
+   * @brief Construction d'un nouvel objet Timer de type t permettant de compter
+   * le nombre de millisecondes qui sont passées
+   * tout en limitant le temps maximum utilisé par images
+   *
+   * @param max_ms_per_frame nombre maximum de millisecondes autorisées par
+   * image
+   * @param t type d'horloge utilisé pour les calculs
+   */
   Timer(u32 max_ms_per_frame, timer_type t);
   ~Timer();
 
-  void start();
   /**
-   * @brief Returns the time passed in ms between start() and stop()
+   * @brief Lance l'horloge et stocke la valeur de l'horloge interne
+   * 
+   */
+  void start();
+
+  /**
+   * @brief retourne le temps passé en ms entre le appels des méthodes start()
+   * et stop()
    *
-   * @return f64
+   * @return f64: valeur du temps passée
    */
   f64 stop();
 
-  f64 get_FPS();
-  void print();
   /**
-   * @brief resets the internal stop_ counter after the variable delay
+   * @brief Retourne le nombre d'images par secondes auquel tourne le moteur,
+   * avec pour limite le nombre maximum de millisecondes autorisées par image
+   *
+   * @return f64: valeur correspondant au nombre d'images par secondes calculées
+   */
+  f64 get_FPS();
+
+  /**
+   * @brief Affiche le nombre d'images par secondes à l'écran de façon lisible
+   *
+   */
+  void print();
+
+  /**
+   * \~english @brief resets the internal stop_ counter after the variable delay
    * has been applied, allows to get the exact delay between 2 frames
    * for FPS caping
+   * \~french @brief Remet à 0 la variable interne stop_ après que le délais
+   * variable à été appliqué, permet d'obtenir le delai exact entre deux images
+   * afin de limiter le nombre d'images par seconde
    *
    */
   f64 variable_delay();
+
   /**
-   * @brief Induces an artificial delay < max_ms_per_frame - 4
+   * \~english @brief Induces an artificial delay < max_ms_per_frame - 4
    * Mainly here to test clock mechanism like FPS caping
-   *
+   * \~french @brief Introduit un délai artificiel à des fins de débogage.
+   * Permets de tester le mécanisme de limitation du nombres d'images par
+   * seconde
    */
   void artificial_delay();
 };
